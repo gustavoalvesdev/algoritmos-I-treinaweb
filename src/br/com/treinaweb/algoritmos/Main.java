5117ce85e24457eb303772f04a14150b99e70133
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2025-02-07 15:10:09 -0300

    Entendendo o que são os arrays

--- src/br/com/treinaweb/algoritmos/Main.java
+++ src/br/com/treinaweb/algoritmos/Main.java
@@ -4,7 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int[] numeros = new int[5];
+		numeros[0] = 22;
+		System.out.println(numeros[0]);
 	}
 
 }
